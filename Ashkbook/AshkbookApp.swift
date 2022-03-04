ca8fc82130cf428c0200b400bf29a58c131d0451
Ashkan Goharfar <ashkan.goharfar@gmail.com>
2022-03-03 20:47:02 -0500

    Initial Commit

---
+++ Ashkbook/AshkbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  AshkbookApp.swift
+//  Ashkbook
+//
+//  Created by Ashkan Goharfar on 2022-03-03.
+//
+
+import SwiftUI
+
+@main
+struct AshkbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Ashkbook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ashkbook
+//
+//  Created by Ashkan Goharfar on 2022-03-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
