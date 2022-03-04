5bd2a2b4c7cb52c83cc7c1461fb192be3b16e23f
Ashkan Goharfar <ashkan.goharfar@gmail.com>
2022-03-03 21:43:36 -0500

    Search bar added.

--- Ashkbook/AshkbookApp.swift
+++ Ashkbook/AshkbookApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct AshkbookApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(text: .constant(""))
         }
     }
 }
--- Ashkbook/ContentView.swift
+++ Ashkbook/ContentView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @Binding var text: String
+    
     // grabbed facebook rgb color value
     let facebookBlue = UIColor(red: 23/255.0,
                                green: 120/255.0,
@@ -42,6 +44,26 @@ struct ContentView: View {
             }
             .padding()
             
+            // First parameter is placeholder
+            // $ is for update the UI based on the text in real time
+            TextField("Search...", text: $text)
+                .padding(7)
+                .background(Color(.systemGray5))
+                .cornerRadius(13)
+                .padding(.horizontal, 15)
+
+            // everything are going to stacked up of each other
+            // Next task add horizontally scrolling stories
+            ZStack {
+                Color(.secondarySystemBackground)
+                
+                ScrollView(.vertical) {
+                    VStack {
+                        
+                    }
+                }
+            }
+            
             Spacer()
         }
     }
@@ -49,6 +71,7 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        // 
+        ContentView(text: .constant(""))
     }
 }
