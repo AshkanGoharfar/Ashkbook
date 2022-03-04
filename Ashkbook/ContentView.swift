4111c2fe5134d765bb4f2cdbcd103e472e3a461f
Ashkan Goharfar <ashkan.goharfar@gmail.com>
2022-03-03 22:13:04 -0500

    Added scrolling stories horizontally.

--- Ashkbook/ContentView.swift
+++ Ashkbook/ContentView.swift
@@ -11,6 +11,8 @@ struct ContentView: View {
     
     @Binding var text: String
     
+    let stories = ["story1", "story2", "story3", "story1", "story2", "story3"]
+    
     // grabbed facebook rgb color value
     let facebookBlue = UIColor(red: 23/255.0,
                                green: 120/255.0,
@@ -53,12 +55,31 @@ struct ContentView: View {
                 .padding(.horizontal, 15)
 
             // everything are going to stacked up of each other
-            // Next task add horizontally scrolling stories
+            // Added scrolling stories horizontally
             ZStack {
                 Color(.secondarySystemBackground)
                 
                 ScrollView(.vertical) {
                     VStack {
+                        
+                        ScrollView(.horizontal, showsIndicators: false) {
+                            HStack(spacing: 3) {
+                                ForEach(stories, id: \.self) { name in
+                                    Image(name)
+                                        // Add aspect ratio to modify the image size to the frame size
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fill)
+                                    
+                                        .frame(width: 140, height: 200, alignment: .center)
+                                        .background(Color.red)
+                                    
+                                        // fianly clipped it here
+                                        .clipped()
+//                                    Spacer()
+                                }
+                            }
+                            .padding()
+                        }
                         
                     }
                 }
